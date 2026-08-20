e4577ce2ba7945b62f7da2caf945e6f8ec18f09e
Davit_Bakradze <79979430+BakradzeDavit@users.noreply.github.com>
2026-08-20 23:53:46 +0400

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public bool hasDuplicate(int[] nums)
+    {
+
+        HashSet<int> result = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (result.Contains(nums[i]))
+            {
+                return true;
+            } else
+            {
+                result.Add(nums[i]);
+            }
+        }
+        return false;
+
+
+    }
+}
